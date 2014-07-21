20aa4b2290cc3ecde53d947afa16d22a4ff33176
unknown <reka.burmeister@TST-REKA.red-gate.com>
2014-07-21 14:04:07 +0100

    Adding an implementation of IFoosPlayer for the main grid's usage

---
+++ FoosNet/FoosPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoosNet.Network;
+
+namespace FoosNet
+{
+    class FoosPlayer : IFoosPlayer
+    {
+        public string Name { get; private set; }
+        public Status Status { get; private set; }
+        public Task<ChallengeResponse> ChallengePlayer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
