598fd5218969808e012e48a196716d3b5c1dc3cf
Martin Podlubny <martin.podlubny@red-gate.com>
2014-07-21 15:01:26 +0100

    first implementation of communicator integration. Ability to check status, and open message window with a contact identified by their redgate email address

---
+++ CommunicatorTestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoosNet.CommunicatorIntegration;
+
+namespace CommunicatorTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommunicatorIntegration ci = new CommunicatorIntegration();
+            ci.OpenConversationWithRedgateEmail(CommunicatorIntegration.NameToRegateEmail("christopher", "moore"));
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ FoosNet.CommunicatorIntegration/CommunicatorIntegration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicatorAPI;
+using FoosNet.Network;
+
+namespace FoosNet.CommunicatorIntegration
+{
+    public class CommunicatorIntegration
+    {
+        private IMessengerAdvanced m_Messenger;
+        private string m_ServiceID;
+
+        public CommunicatorIntegration()
+        {
+            m_Messenger = (IMessengerAdvanced) new Messenger();
+            m_ServiceID = m_Messenger.MyServiceId;
+        }
+
+        public Status StatusOfRedgateEmail(string email)
+        {
+            IMessengerContact contact = GetContactByRedGateEmail(email);
+            switch (contact.Status)
+            {
+                case MISTATUS.MISTATUS_ONLINE:
+                case MISTATUS.MISTATUS_MAY_BE_AVAILABLE:
+                    return Status.Available;
+                case MISTATUS.MISTATUS_OFFLINE:
+                    return Status.Offline;
+                default:
+                    return Status.Busy;
+            }
+        }
+
+        public IMessengerConversationWndAdvanced OpenConversationWithRedgateEmail(string email)
+        {
+            return m_Messenger.InstantMessage(GetContactByRedGateEmail(email));
+        }
+
+        public static string NameToRegateEmail(string first, string last)
+        {
+            return first + "." + last + "@red-gate.com";
+        }
+
+        private dynamic GetContactByRedGateEmail(string email)
+        {
+            return m_Messenger.GetContact(email, m_ServiceID);
+        }
+    }
+}
