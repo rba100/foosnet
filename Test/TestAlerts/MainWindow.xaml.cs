6c880c80b78afe283a23502e97d86a5796d6ab6d
Oli Lane <ojgl2@cam.ac.uk>
2014-07-21 14:42:27 +0100

    TestAlerts project
    Currently just spams a "FOOS ALERT" window full screen onto every
    monitor

---
+++ Test/TestAlerts/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace TestAlerts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var screenCount = Screen.AllScreens.Count();
+
+            foreach (var screen in Screen.AllScreens)
+            {
+                var window = new AlertWindow
+                {
+                    Top = screen.WorkingArea.Top,
+                    Left = screen.WorkingArea.Left,
+                    Width = screen.WorkingArea.Width,
+                    Height = screen.WorkingArea.Height
+                };
+                window.Show();
+                window.Activate();
+                window.WindowState = WindowState.Maximized;
+            }
+            
+            TestWindow.Hide();
+
+        }
+    }
+}
