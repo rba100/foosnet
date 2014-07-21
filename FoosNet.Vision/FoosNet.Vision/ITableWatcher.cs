a7e7ac4c258952e765d41597f38f675fc9a8e95f
Jason Crease <jason.crease@red-gate.com>
2014-07-21 14:12:45 +0100

    Initial commit of Foos Vision bits

---
+++ FoosNet.Vision/FoosNet.Vision/ITableWatcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoosNet.Vision
+{
+    public interface ITableWatcher
+    {
+        bool TableIsInUse { get; }
+        event EventHandler TableHasBecomeInUse;
+        event EventHandler TableHasBecomeFree;
+    }
+}
