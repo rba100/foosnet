a1c39c8e7b218de140f00045dbd0a936f095ec3e
unknown <reka.burmeister@TST-REKA.red-gate.com>
2014-07-21 15:35:32 +0100

    Adding a class that converts the player status to an image

---
+++ FoosNet/ConvertStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using FoosNet.Network;
+
+namespace FoosNet
+{
+    class ConvertStatus : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Status status = (Status)value;
+            switch (status)
+            {
+                case Status.Available:
+                    return "Images/GreenOrb.ico";
+                case Status.Busy:
+                    return "Images/AmberOrb.ico";
+                case Status.Offline:
+                    return "Images/RedOrb.ico";
+                case Status.Unknown:
+                    return "Images/BlueOrb.ico";
+                case Status.Useless:
+                    return "Images/BlueOrb.ico";
+                default:
+                    throw new Exception("Set new status!");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
