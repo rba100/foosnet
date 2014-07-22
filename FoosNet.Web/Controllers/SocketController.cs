312247121cb7ed58b1fe13c7a8fc8ff128075a42
Mark Raymond <mark.raymond@red-gate.com>
2014-07-22 16:39:49 +0100

    Add web project

---
+++ FoosNet.Web/Controllers/SocketController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using FoosNet.Web.Handlers;
+using Microsoft.Web.WebSockets;
+
+namespace FoosNet.Web.Controllers
+{
+    public class SocketController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            if (HttpContext.Current.IsWebSocketRequest || HttpContext.Current.IsWebSocketRequestUpgrading)
+            {
+                HttpContext.Current.AcceptWebSocketRequest(new SocketHandler());
+                return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ FoosNet.Web/Handlers/WebSocketHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web.Helpers;
+using Microsoft.Web.WebSockets;
+
+namespace FoosNet.Web.Handlers
+{
+    public class SocketHandler : WebSocketHandler, IDisposable
+    {
+        private static readonly ConcurrentDictionary<string, SocketHandler> s_Subscribers = new ConcurrentDictionary<string, SocketHandler>();
+        private string m_Email = null;
+
+        public override void OnMessage(string message)
+        {
+            var m = Json.Decode(message);
+            var action = m.action as string;
+            switch (action)
+            {
+                case "subscribe":
+                    Subscribe(m.email);
+                    break;
+                case "challenge":
+                    Challenge(m.toChallenge);
+                    break;
+                case "respond":
+                    Respond(m.challenger, m.response);
+                    break;
+                case "players":
+                    Players();
+                    break;
+                default:
+                    throw new InvalidOperationException(string.Format("Unknown action \"{0}\"", action));
+            }
+        }
+
+        public override void OnClose()
+        {
+            Dispose();
+            base.OnClose();
+        }
+
+        private void Subscribe(string email)
+        {
+            if (m_Email != null && m_Email != email) throw new InvalidOperationException("Cannot change email address during a connection");
+            m_Email = email;
+            s_Subscribers[email] = this;
+            Broadcast(new { type = "player", player = email });
+        }
+
+        private void Challenge(string toChallenge)
+        {
+            SendTo(toChallenge, new { type = "challenge", challengedBy = m_Email });
+        }
+
+        private void Respond(string challengedBy, string response)
+        {
+            SendTo(challengedBy, new { type = "response", player = m_Email, response = response });
+        }
+
+        private void Players()
+        {
+            Send(new { type = "players", players = s_Subscribers.Keys.Where(e => e != m_Email).ToArray() });
+        }
+
+        private void Broadcast(object message)
+        {
+            var sendTo = s_Subscribers.Values.Where(s => s != this).ToList();
+            foreach (var recipient in sendTo)
+            {
+                recipient.Send(message);
+            }
+        }
+
+        private void SendTo(string to, object message)
+        {
+            s_Subscribers[to].Send(message);
+        }
+
+        private void Send(object message)
+        {
+            base.Send(Json.Encode(message));
+        }
+
+        public void Dispose()
+        {
+            SocketHandler removed;
+            s_Subscribers.TryRemove(m_Email, out removed);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SocketHandler()
+        {
+            Dispose();
+        }
+    }
+}
