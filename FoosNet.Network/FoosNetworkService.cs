eb2c88ec34df256d45b0262bb4aa0967c8243a1e
Robin Anderson <robin@rbanderson.co.uk>
2014-07-22 12:07:19 +0100

    foos network service class

--- FoosNet.Network/FoosNetworkService.cs
+++ FoosNet.Network/FoosNetworkService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FoosNet.Network;
-
-namespace FoosNet.Services
+﻿namespace FoosNet.Network
 {
     public class FoosNetworkService
     {
