170fbbd644d72d4a5cfc3cc3c94c2044f3fdff6d
Oli Lane <ojgl2@cam.ac.uk>
2014-07-22 16:28:03 +0100

    Removed unused references

--- FoosNet/NotifyWindowViewModel.cs
+++ FoosNet/NotifyWindowViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IdentityModel.Tokens;
 using System.Net.Mime;
 using System.Runtime.CompilerServices;
 using System.Windows.Documents;
