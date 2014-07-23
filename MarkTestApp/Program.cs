99ec8bceb25a9c09fe0d35ce48996b6befed0655
Mark Raymond <mark.raymond@red-gate.com>
2014-07-23 11:51:38 +0100

    Test player discovery

--- MarkTestApp/Program.cs
+++ MarkTestApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FoosNet.Network;
@@ -10,7 +11,10 @@ namespace MarkTestApp
         static void Main()
         {
             IFoosNetworkService foosnet1 = new FoosNetworkService("ws://mdr-foosnet.testnet.red-gate.com/Api/Socket", "mark.raymond@red-gate.com");
+            Thread.Sleep(400);
             IFoosNetworkService foosnet2 = new FoosNetworkService("ws://mdr-foosnet.testnet.red-gate.com/Api/Socket", "robin.anderson@red-gate.com");
+            foosnet1.PlayersDiscovered += players => Console.WriteLine("Mark discovered {0}", string.Join(", ", players.Players.Select(p => p.Email)));
+            foosnet2.PlayersDiscovered += players => Console.WriteLine("Robin discovered {0}", string.Join(", ", players.Players.Select(p => p.Email)));
             foosnet2.ChallengeReceived += request => Task.Factory.StartNew(() =>
             {
                 Console.WriteLine("Robin recieved challenge");
