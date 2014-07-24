d7e6ca31d41968f9ade6c689936c3f84dda4a4a7
Mark Raymond <mark.raymond@red-gate.com>
2014-07-24 11:39:50 +0100

    AssemblyInfo,.cs

--- FoosNet.Web/Properties/AssemblyInfo.cs
+++ FoosNet.Web/Properties/AssemblyInfo.cs
@@ -1,35 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("FoosNet.Web")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Red Gate")]
-[assembly: AssemblyProduct("FoosNet.Web")]
+[assembly: AssemblyProduct("FoosNet")]
 [assembly: AssemblyCopyright("Copyright © Red Gate 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c0e42669-4d99-49d4-a069-7fe7c10d280c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
